67bd4d14d5a4141b534865fbac50babef48a2545
lfeliperibeiro <piporibeiro@gmail.com>
2020-10-12 13:26:20 -0300

    feat: create orphanage component

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {FiPlus} from 'react-icons/fi'
+
+import mapMakerImg from '../images/Map-mark.svg'
+
+import '../styles/pages/orphanages-map.css'
 
 function OrphanagesMap () {
   return (
-    <h1>Hello</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMakerImg} alt="Happy"/>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </footer>
+
+      </aside>
+      <div></div>
+
+      <Link to ="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
   )
 }
 
