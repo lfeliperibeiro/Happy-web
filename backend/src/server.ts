ae53dd20d0931e30c84cd3dda88bb283b3c93cb8
lfeliperibeiro <piporibeiro@gmail.com>
2020-10-13 18:32:19 -0300

    refactor: change files for MVC method

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,38 +1,13 @@
 import express from 'express'
-import {getRepository} from 'typeorm'
-import Orphanage from './models/Orphanage'
-
 import './database/connection'
+
+import routes from './routes'
 
 const app = express() 
 
 app.use(express.json())
+app.use(routes)
 
-app.post('/orphanages', async (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = req.body
 
-  const orphanagesRepository = getRepository(Orphanage)
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  })
-
-  await orphanagesRepository.save(orphanage)
-  return res.json({message: 'Hello World!'})
-})
 
 app.listen(3333)
