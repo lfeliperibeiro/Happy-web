ede7b7aa2efdd16a3f0a1e20e7fbc5253b279f65
lfeliperibeiro <piporibeiro@gmail.com>
2020-10-13 19:40:41 -0300

    feat: config multer of receiver images

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express'
+import {request, Request, Response} from 'express'
 import {getRepository} from 'typeorm'
 import Orphanage from '../models/Orphanage'
 
@@ -30,6 +30,12 @@ export default {
     } = req.body
   
     const orphanagesRepository = getRepository(Orphanage)
+
+    const requestImages = req.files as Express.Multer.File[]
+    const images = requestImages.map(image => {
+      return {path: image.filename}
+    })
+
   
     const orphanage = orphanagesRepository.create({
       name,
@@ -38,7 +44,8 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
+      open_on_weekends,
+      images
     })
   
     await orphanagesRepository.save(orphanage)
